34c2fe2e5373b19514a4f894bce168f6e9360012
maVovk <MichaelVovk@yandex.ru>
2022-12-13 15:43:34 +0300

    validation, update, delete added

--- api/api_routes.py
+++ api/api_routes.py
@@ -6,8 +6,6 @@ from models.User import User
 from models.Task import Task
 from models.Goal import Goal
 
-from functools import wraps
-
 from jwt.exceptions import DecodeError, ExpiredSignatureError
 
 
@@ -17,7 +15,7 @@ SECRET_KEY = app.config['SECRET_KEY']
 
 @app.before_request
 def before_request():
-    if request.path == '/api/login' or request.path == '/api/register':
+    if request.path == '/api/login' or request.path == '/api/register' or request.method.lower() == 'options':
         return
 
     token = request.headers.get('Authorization')
@@ -77,9 +75,13 @@ def get_tasks():
     return {'tasks': [task.to_dict() for task in tasks]}
 
 
-@app.route('/api/tasks/<int:user_id>', methods=['GET'])
-def get_tasks_by_user(user_id):
-    tasks = Task.query.filter_by(user_id=user_id).all()
+@app.route('/api/get_tasks', methods=['GET'])
+def get_tasks_by_user():
+    token = request.headers.get('Authorization')
+    data = jwt.decode(token, SECRET_KEY)
+    user = User.query.filter_by(username=data['username']).first()
+
+    tasks = Task.query.filter_by(user_id=user.id).all()
     return {'tasks': [task.to_dict() for task in tasks]}
 
 
@@ -98,9 +100,29 @@ def create_goal(label, task_id):
     db.session.add(goal)
 
 
+def validate_task(data):
+    if 'title' not in data or 'description' not in data or 'goals' not in data:
+        return 'Missing data', 400
+
+    if len(data['title']) > 80 or len(data['description']) > 120:
+        return 'Data is too long', 400
+
+    if not data['goals']:
+        return 'Goals cannot be empty', 400
+
+    for goal in data['goals']:
+        if len(goal) > 80:
+            return 'Goal is too long', 400
+        if not len(goal):
+            return 'Goal cannot be empty', 400
+
+    return None
+
+
 @app.route('/api/create_task', methods=['POST'])
 def create_task():
     data = request.get_json()
+    validate_task(data)
 
     token = request.headers.get('Authorization')
     decoded = jwt.decode(token, SECRET_KEY)
@@ -117,7 +139,7 @@ def create_task():
 
     db.session.commit()
 
-    return {'task_id': task.id}, 200
+    return task.to_dict(), 200
 
 
 @app.route('/api/update_task/<int:task_id>', methods=['PUT'])
@@ -140,13 +162,35 @@ def update_task(task_id):
 def update_goal():
     data = request.get_json()
 
-    for goal_id, status in data['goals']:
+    for goal_id, status in data:
         goal = Goal.query.filter_by(id=goal_id).first()
 
         if goal is None:
             continue
 
         goal.done = bool(status)
+
+    db.session.commit()
+
+    return '', 200
+
+
+@app.route('/api/delete_task', methods=['DELETE'])
+def delete_task():
+    data = request.get_json()
+
+    task = Task.query.filter_by(id=data).first()
+
+    if task is None:
+        return 'Task does not exist', 404
+
+    goals = Goal.query.filter_by(task_id=task.id).all()
+
+    for goal in goals:
+        db.session.delete(goal)
+
+    db.session.commit()
+    db.session.delete(task)
 
     db.session.commit()
 
